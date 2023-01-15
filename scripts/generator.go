f7a6a3fbeffe9917a572b884e27d2bd181a505d0
Sadegh <me@sadegh.io>
2023-01-15 16:27:35 +0330

    fix: expose `PortRange` function

--- scripts/generator.go
+++ scripts/generator.go
@@ -2,14 +2,14 @@ package scripts
 
 import "fmt"
 
-// portRange generates port range rules with specified protocol.
-// portRange("TCP", [][2]int{{10000, 10019}})
+// PortRange generates port range rules with specified protocol.
+// PortRange("TCP", [][2]int{{10000, 10019}})
 // result:
 // AND,((DEST-PORT,10000), (PROTOCOL,TCP))
 // AND,((DEST-PORT,10001), (PROTOCOL,TCP))
 // ...
 // // AND,((DEST-PORT,10019), (PROTOCOL,TCP))
-func portRange(protocol string, ranges [][2]int) []string {
+func PortRange(protocol string, ranges [][2]int) []string {
 	var rules []string
 
 	for _, ports := range ranges {
